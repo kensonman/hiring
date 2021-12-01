207cf5cb845c4631c8d5c3c9b8f9d3815664dca3
Kenson Man <kenson@kenson.idv.hk>
2021-12-01 15:47:31 +0000

    Create the model and translation

--- API/src/meter/admin.py
+++ API/src/meter/admin.py
@@ -1,3 +1,25 @@
+# -*- coding: utf-8 -*-
+# File: src/meter/admin.py
+# Date: 2021-12-01 15:31
+# Author: Kenson Man <kenson@kenson.idv.hk>
+# Usage: Provide the admin-tools for debug and testing
 from django.contrib import admin
+from .models import *
 
-# Register your models here.
+@admin.register(Reading)
+class ReadingAdmin(admin.ModelAdmin):
+   fields         = ('id', 'customerId', 'serialNumber', 'meterType', 'readingType', 'registerId', 'value', 'readDate')
+   list_display   = ('id', 'customerId', 'serialNumber', 'meterType', 'readingType', 'registerId', 'value', 'readDate')
+   list_filter    = ('customerId', 'serialNumber', 'meterType', 'readingType', 'registerId', 'readDate')
+   ordering       = ('-readDate',)
+   readonly_fields=('id', 'readDate')
+   search_field   =('id', 'customerId', 'serialNumber', 'meterType', 'readingType', 'registerId', 'value', 'readDate' )
+
+@admin.register(History)
+class HistoryAdmin(admin.ModelAdmin):
+   fields         = ('id', 'data', 'ipaddr', 'date', 'stored')
+   list_display   = ('id', 'data', 'ipaddr', 'date', 'stored')
+   list_filter    = ('ipaddr', 'date')
+   ordering       = ('-date',)
+   readonly_fields=('id', 'date', 'stored')
+   search_field   =('id', 'data', 'ipaddr', 'date', 'stored')
--- API/src/meter/models.py
+++ API/src/meter/models.py
@@ -1,3 +1,67 @@
+# -*- coding: utf-8 -*-
+# File: src/meter/model.py
+# Date: 2021-12-01 15:08
+# Author: Kenson Man <kenson@kenson.idv.hk>
+# Usage: Create the data-model for the bluetel/hiring/API 
 from django.db import models
+from django.utils.translation import ugettext_lazy as _, ugettext as __
+import uuid
 
-# Create your models here.
+class Reading(models.Model):
+   '''
+   The model to store the reading that submitted by user/meter. It is also used for query the result.
+
+   Provided Schema
+   ====
+   {
+      "customerId": "identifier123",
+      "serialNumber": "27263927192",
+      "mpxn": "14582749",
+      "read": [
+         {"type": "ANYTIME", "registerId": "387373", "value": "2729"},
+         {"type": "NIGHT", "registerId": "387373", "value": "2892"}
+      ],
+      "readDate": "2017-11-20T16:19:48+00:00Z"
+   }
+
+   I had denormalized the "read" property. It will make the program lighter and easy to maintenance.
+   '''
+   class Meta(object):
+      verbose_name         =_('meter.Reading')
+      verbose_name_plural  =_('meter.Readings')
+
+   METER_ELECTRIC = 0
+   METER_GAS      = 1
+   METER_TYPES    = (
+      (METER_ELECTRIC, _('meter.types.electric')),
+      (METER_GAS, _('meter.types.gas')),
+   )
+
+   id             = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False, verbose_name=_('meter.Reading.id'), help_text=_('meter.Reading.id.helptext'))
+   customerId     = models.CharField(max_length=50, verbose_name=_('meter.Reading.customerId'), help_text=_('meter.Reading.customerId.helptext'))
+   serialNumber   = models.CharField(max_length=50, verbose_name=_('meter.Reading.serialNumber'), help_text=_('meter.Reading.serialNumber.helptext'))
+   meterType      = models.IntegerField(choices=METER_TYPES, default=METER_ELECTRIC, verbose_name=_('meter.Reading.meterType'), help_text=_('meter.Reading.meterType.helptext'))
+   readingType    = models.CharField(max_length=20, verbose_name=_('meter.Reading.readingType'), help_text=_('meter.Reading.readingType.helptext'))
+   registerId     = models.CharField(max_length=20, verbose_name=_('meter.Reading.registerId'), help_text=_('meter.Reading.registerId.helptext'))
+   value          = models.IntegerField(verbose_name=_('meter.Reading.value'), help_text=_('meter.Reading.value.helptext'))
+   readDate       = models.DateTimeField(auto_now=True, verbose_name=_('meter.Reading.readDate'), help_text=_('meter.Reading.readDate.helptext'))
+
+class History(models.Model):
+   '''
+   The model to store as the history or audit check.
+
+   In the business envirnonment, the raw-data is so important and we cannot accept any error on it. So that I created this model
+   to store all the raw-data (that user just submit the data). It also provide the ability to found out the issue if there have any 
+   error/unexpected result.
+
+   The id is also used for idempotency check to reduce any re-submit issue.
+   '''
+   class Meta(object):
+      verbose_name         =_('meter.History')
+      verbose_name_plural  =_('meter.Histories')
+
+   id             = models.CharField(max_length=100, primary_key=True, verbose_name=_('meter.History.id'), help_text=_('meter.History.id.helptext'))
+   data           = models.JSONField(verbose_name=_('meter.History.data'), help_text=_('meter.History.data.helptext'))
+   ipaddr         = models.CharField(max_length=40, verbose_name=_('meter.History.ipaddr'), help_text=_('meter.History.ipaddr.helptext'))
+   date           = models.DateTimeField(auto_now=True, verbose_name=_('meter.History.date'), help_text=_('meter.History.date.helptext'))
+   stored         = models.ForeignKey(Reading, null=True, blank=True, verbose_name=_('meter.History.stored'), help_text=_('meter.History.stored.helptext'), on_delete=models.SET_NULL)
